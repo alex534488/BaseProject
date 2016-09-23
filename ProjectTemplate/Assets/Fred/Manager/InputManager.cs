1b3646b59430e6b57834fc08ead29b2427a59fbc
Fred Bessette <msfredb7@gmail.com>
2016-09-23 16:52:12 -0400

    Project Fix

---
+++ ProjectTemplate/Assets/Fred/Manager/InputManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace CCC.Manager
+{
+    public class InputManager : BaseManager
+    {
+        public class KeySave
+        {
+            public string keyName;
+            public KeyCode keycode;
+
+            public KeySave(string name, KeyCode keycode)
+            {
+                this.keyName = name;
+                this.keycode = keycode;
+            }
+        }
+
+        [System.Serializable]
+        public class SaveClass
+        {
+            public List<KeySave> keySaves = new List<KeySave>();
+
+            public KeySave Get(Key key)
+            {
+                foreach(KeySave keySave in keySaves)
+                {
+                    if (keySave.keyName == key.GetName()) return keySave;
+                }
+                return null;
+            }
+        }
+
+        public InputBank bank;
+        SaveClass save;
+
+        /// <summary>
+        /// Set the specified key to the specified keycode. This does not save the change permanently. Call Save() if necessary.
+        /// </summary>
+        public void SetKey(Key key, KeyCode to)
+        {
+            //Vérifier si la save existe déjà, si oui, la remplacer
+            KeySave keySave = save.Get(key);
+            if(key == null)
+            {
+                keySave = new KeySave(key.GetName(), to);
+                save.keySaves.Add(keySave);
+            }
+            else
+            {
+                keySave.keycode = to;
+            }
+
+            key.SetKeyCode(to);
+        }
+
+        public void SetDefault()
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
