b63080950da13c9d99ee21cc8f37bdf74d11e620
Xeal <alexandre.blanchet.94@gmail.com>
2016-09-29 16:44:21 -0400

    Script OnEntityClick de base termine

---
+++ ZombieTown/Assets/Game/OnEntityClick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class OnEntityClick : MonoBehaviour
+{
+    public static OnEntityClick selectedChar = null; // Personnage presentement selectionner
+    public Personnage character; // Personnage ou se trouve ce script
+    public GameObject selectAnimation;
+    public bool clickable = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // If right click
+        {
+            //character.comportement.currentStates.MoveTo();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (!Input.GetMouseButtonDown(0)){ return; }
+
+        if (clickable)
+        {
+            if (selectedChar != character)
+            {
+            selectedChar = this;
+            selectedChar.UnSelect();
+            //selectAnimation.SetActive(true);
+            }   
+        }
+    }
+
+    public void UnSelect()
+    {
+        //selectAnimation.SetActive(false);
+    }
+}
