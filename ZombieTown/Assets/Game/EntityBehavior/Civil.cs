a048b761b5d0e62d890cbb8f2ff5716baaf386dc
Xeal <alexandre.blanchet.94@gmail.com>
2016-10-02 22:19:56 -0400

    Ajout de la fonctionnalite Follow des zombies

--- ZombieTown/Assets/Game/EntityBehavior/Civil.cs
+++ ZombieTown/Assets/Game/EntityBehavior/Civil.cs
@@ -7,6 +7,9 @@ public class Civil : Personnage
 {
     public GameObject sprite;
     public Vector2 mapSize;
+    private Vector3 fleetarget;
+
+    public UnityEvent onZombieNearby;
 
     // Use this for initialization
     void Start()
@@ -18,7 +21,35 @@ public class Civil : Personnage
 
         // Set Initial Behaviors
         comportement.ChangeState<StatesIdle>();
+
+        //Setup detector
+        if (detector != null)
+        {
+            System.Type[] enemies =
+            {
+                typeof(Zombie)
+            };
+            System.Type[] allies =
+            {
+                //On ne met aucun allié parce que les humans (policier / civile) n'intéragisse pas ensemble pour l'instant
+            };
+            detector.Init(enemies, true, allies, false);
+            detector.onEnemyEnter.AddListener(OnZombieNearby);
+        }
     }
 
+    void Update()
+    {
+        if(comportement.currentStates.target != null)
+        {
+            // calculer la position la plus propice a une fuite
+            // et l'enregistrer dans fleetarget
+        }
+    }
 
+    void OnZombieNearby(Personnage personnage)
+    {
+        comportement.ChangeState<StatesMoveTo>();
+        (comportement.currentStates as StatesMoveTo).Init(fleetarget);
+    }
 }
