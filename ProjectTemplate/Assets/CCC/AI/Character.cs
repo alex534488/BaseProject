c4667e9c069bab6a27cf64657086842f45dd5a88
Xeal <alexandre.blanchet.94@gmail.com>
2016-10-05 10:25:35 -0400

    Ajout de fonctionnalites d'AI et de machines a Etats finit au projet de base ainsi qu'une classe generique de projectiles

---
+++ ProjectTemplate/Assets/CCC/AI/Character.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class Character : Personnage
+{
+    // Attribut
+    // ex: public float aurasize;
+
+    // Evennements
+    // ex: public UnityEvent onZombieNearby;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set Variables
+        damage = 1;
+        hp = 1;
+
+        // Set Initial Behaviors
+        ChangeState<StatesIdle>();
+
+        // Setup Entity detector
+        /* Ex: 2D Detector Version
+        if (detector != null)
+        {
+            System.Type[] enemies =
+            {
+                typeof(Zombie)
+            };
+            System.Type[] allies =
+            {
+                //On ne met aucun allié parce que les humans (policier / civile) n'intéragisse pas ensemble pour l'instant
+            };
+            detector.Init(enemies, true, allies, false);
+            detector.onEnemyEnter.AddListener(OnEnemyNearby);
+        }
+        */
+
+        // Setup Events
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    ///////////////////////
+
+    // Ajouter fonctions specifique aux attributs ou aux character
+}
