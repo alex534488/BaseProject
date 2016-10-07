6e784631303c7c92e4f70a8224beb63367576d0a
Xeal <alexandre-blanchet94@hotmail.com>
2016-10-07 18:04:54 -0400

    Ajout de fonctionnalites

--- GameJam2016/Assets/Game/Scripts/Empire.cs
+++ GameJam2016/Assets/Game/Scripts/Empire.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
 
 
-public class Empire : World {
+public class Empire : MonoBehaviour {
 
+    List<Village> listVillage = new List<Village>();
+    Capitale capitale;
 
 	void Start () {
 	
