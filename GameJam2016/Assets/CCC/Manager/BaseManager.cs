9436243a4706bfce90f74070056e15f077cead0d
Fred Bessette <msfredb7@gmail.com>
2016-10-06 23:02:06 -0400

    GameJam directory

---
+++ GameJam2016/Assets/CCC/Manager/BaseManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+namespace CCC.Manager
+{
+    public class BaseManager : MonoBehaviour
+    {
+        public class BaseManagerEvent: UnityEvent<BaseManager> { }
+        [HideInInspector]
+        public BaseManagerEvent onCompleteInit = new BaseManagerEvent();
+        [HideInInspector]
+        public bool initComplete = false;
+
+        protected virtual void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public virtual void Init()
+        {
+
+        }
+
+        protected void CompleteInit()
+        {
+            initComplete = true;
+            onCompleteInit.Invoke(this);
+        }
+    }
+}
