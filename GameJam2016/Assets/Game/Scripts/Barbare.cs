28e0d1a8fe0f270a69613ebba53d7dcf4392227a
eboucher4 <eric_2212@hotmail.ca>
2016-10-07 18:04:48 -0400

    Barbare et World

--- GameJam2016/Assets/Game/Scripts/Barbare.cs
+++ GameJam2016/Assets/Game/Scripts/Barbare.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Barbare : MonoBehaviour {
+public class Barbare : MonoBehaviour
+{
+    public World theWorld;
+    private Village actualTarget;
+    private int nbBarbares;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start(){}
+
+    void Update() {}
+
+    void AskTarget() // Retourne le village frontiere avec le moins de soldats disponibles
+    {
+        actualTarget = theWorld.GiveTarget(); 
+    }
+
+    void WaitForAttack(int nbTours) // Attend un certain nombre de tours avant de attaquer
+    {
+        // Ajoute un Listener sur la fonction de fin de tour
+        // Chaque fois que elle se declenche, decroit le compteur de 1
+        TakeDecision(); // Appelle la fonction TakeDecision qui determinera si les barbares attaqueront le village;
+    }
+
+    void SpawnEnnemy()
+    {
+        // Rajoute 1 au nombre de barbares disponibles
+    }
+
+    void TakeDecision()
+    {
+        // Recupere le nombre de soldats disponible dans le village
+        // Commence à 50% de chance de réussite, si 10% plus de unites cela implique 10% plus de chance de gagner la bataille ?
+        
+    }
+
 }
--- GameJam2016/Assets/Game/Scripts/World.cs
+++ GameJam2016/Assets/Game/Scripts/World.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class World : MonoBehaviour {
 
+    private List<Village> listeVillage;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    public Village GiveTarget() // Verifie la liste des villages et retourne le village frontiere le plus faible de la liste
+    {
+        Village unVillage = null;
+        return unVillage;
+    }
 }
