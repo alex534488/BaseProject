b4b2838ba3e3a376583897dd41297042f85348d1
Fred Bessette <msfredb7@gmail.com>
2016-10-08 02:31:43 -0400

    Next day button

--- GameJam2016/Assets/Game/Scripts/DayManager.cs
+++ GameJam2016/Assets/Game/Scripts/DayManager.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DayManager : MonoBehaviour{
 
     public static DayManager main;
 
+    public Button nextDayButton;
     public World theWorld;
     public RequestManager requestManager;
 
     void Awake()
     {
         if (main == null) main = this;
+        nextDayButton.onClick.AddListener(NextDay);
     }
 
     public void NextDay()
     {
+        print("Next day!");
         requestManager.NewDay();
         
         // Display Bouton Next Day
