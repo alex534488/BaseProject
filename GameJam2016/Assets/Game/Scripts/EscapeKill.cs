ef98d33c324b9bdbb18f2531acc96876b45d661e
Fred Bessette <msfredb7@gmail.com>
2016-10-08 03:42:43 -0400

    Fonts + ui box + character test

---
+++ GameJam2016/Assets/Game/Scripts/EscapeKill.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeKill : MonoBehaviour {
+    
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+	}
+}
