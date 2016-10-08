8c392370be5e03fb2b7ef9dcac2a2ad2582fcac1
eboucher4 <eric_2212@hotmail.ca>
2016-10-08 00:21:37 -0400

    Day Manager

---
+++ GameJam2016/Assets/Game/Scripts/DayManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayManager : IUpdate {
+
+    public World theWorld;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+
+
+        theWorld.Update();
+
+	}
+
+
+
+}
