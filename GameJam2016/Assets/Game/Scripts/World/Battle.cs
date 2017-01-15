a9e94cc48e856addfa84a52b3d02764da70fbd0f
Fred Bessette <msfredb7@gmail.com>
2017-01-15 17:33:49 -0500

    J'ai changé un peu le role de Battle / BattleSetting / BattleResult. BattleResult -> vue que c'est littéralement juste une structure de donnée, je l'ai mis en struct BattleSetting -> je l'ai renommé à Battle, et elle possède maintenant la fonction Resolve() qui retourne un BattleResult Battle -> je l'ai renommée BattleLauncher, ya seulement des fonction statique utile pour réalisé un combat facilement.

--- GameJam2016/Assets/Game/Scripts/World/Battle.cs
+++ GameJam2016/Assets/Game/Scripts/World/Battle.cs
@@ -3,19 +3,27 @@ using System.Collections;
 
 public class Battle {
 
-    BattleSetting battleSetting;
-    BattleResult battleResult;
+    public int invaderPower = 0;
+    public int defenderPower = 0;
+    public float invaderBuff = 1;
+    public float defenderBuff = 1;
 
-    Battle(BattleSetting battleSetting)
+    public Battle()
     {
-        this.battleSetting = battleSetting;
-        battleResult = new BattleResult(battleSetting.GetInvaderPower(),battleSetting.GetDefenderPower());
+
     }
-	
-	public BattleResult LaunchBattle()
-    {
-        // Resoudre la bataille et enregistrer le resultat dans battleResult
 
-        return battleResult;
+    public Battle(int invaderPower, int defenderPower, float invaderBuff = 1, float defenderBuff = 1)
+    {
+        this.invaderPower = invaderPower;
+        this.defenderPower = defenderPower;
+        this.invaderBuff = invaderBuff;
+        this.defenderBuff = defenderBuff;
+    }
+
+    //TODO: mettre les formule de combat
+    public BattleResult Resolve()
+    {
+        return new BattleResult();
     }
 }
--- GameJam2016/Assets/Game/Scripts/World/BattleResult.cs
+++ GameJam2016/Assets/Game/Scripts/World/BattleResult.cs
@@ -1,37 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BattleResult {
+public struct BattleResult {
 
-    private int invaderLeft;
-    private int invaderDeath;
-    private int defenderLeft;
-    private int defenderDeath;
-    private bool defenceWin;
-
-    public BattleResult(int invaderLeft, int defenderLeft)
-    {
-        this.invaderLeft = invaderLeft;
-        this.defenderLeft = defenderLeft;
-        invaderDeath = 0;
-        defenderDeath = 0;
-        defenceWin = true; // la defence a gagner sauf si l'attaquant reussit a le battre?
-    }
-
-	public void AddInvaderDeaths(int amount)
-    {
-        invaderLeft -= amount;
-        invaderDeath += amount;
-    }
-
-    public void AddDefenderDeaths(int amount)
-    {
-        defenderLeft -= amount;
-        defenderDeath += amount;
-    }
-
-    public void InvaderWin()
-    {
-        defenceWin = false;
-    }
+    int invaderLeft;
+    int invaderDeath;
+    int defenderLeft;
+    int defenderDeath;
+    bool defenceWin;
 }
